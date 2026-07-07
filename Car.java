984564caf1ce4441c585e5989abc576f6bccaac5
Joseph Nunez <jnunez110@mail.valenciacollege.edu>
2026-07-06 22:12:42 -0400

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,32 @@
+public class Car {
+
+    private String Make, Model, Country;
+    private int TopSpeed, Horsepower;
+
+    public Car(String make, String model, String country, int topSpeed, int horsepower){
+        this.Make = make;
+        this.Model = model;
+        this.Country = country;
+        this.Horsepower = horsepower;
+        this.TopSpeed = topSpeed;
+    }
+
+    public String outputInfo(){
+        return String.format("%s %s (%s) - Top Speed: %d mph, Hosrepower: %d hp", Make, Model, Country, TopSpeed, Horsepower);
+    }
+    public String getMake() {
+        return Make;
+    }
+    public String getModel() {
+        return Model;
+    }
+    public String getCountry() {
+        return Country;
+    }
+    public double getTopSpeed() {
+        return TopSpeed;
+    }
+    public double getHorsepower() {
+        return Horsepower;
+    }
+}
